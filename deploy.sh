27a5ed58bfcd4ba683d2e3706fc8a93238dfc638
zhangbo <xiaobo1128@gmail.com>
2020-11-27 00:42:55 +0800

    chore: update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,16 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+# mkdir public
+# cd public
+# git init
+# git remote add upstream https://github.com/jdhao/jdhao.github.io.git
+
 # Build the project.
-hugo -t even # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
 cd public
-
-git init
-git remote add upstream git@github.com:philipbo/philipbo.github.io.git
 
 # Add changes to git.
 git add .
@@ -19,12 +21,11 @@ msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
+
 git commit -m "$msg"
 
 # Push source and build repos.
-git push upstream master -f
+git push upstream master
 
 # Come Back up to the Project Root
 cd ..
-
-rm -rf public
